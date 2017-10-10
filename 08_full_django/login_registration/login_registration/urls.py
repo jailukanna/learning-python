2fa70b3b4ce71ff06e1d99c7cd886449721ebf50
twknab <natureminded@users.noreply.github.com>
2017-10-10 01:06:47 -0700

    Deleted old login/registration assignment where request variable was not properly protected.

--- 08_full_django/login_registration/login_registration/urls.py
+++
@@ -1,9 +0,0 @@
-"""login_registration URL Configuration
-
-Sets up all URL configurations for all applications belonging to `login_registration` project.
-"""
-from django.conf.urls import url, include
-
-urlpatterns = [
-    url(r'^', include("apps.logreg.urls")), # Grabs `urls.py` from `./../apps/loginreg` application
-]
